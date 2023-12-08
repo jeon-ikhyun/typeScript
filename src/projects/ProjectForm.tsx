237d1a1794a12371709755ddd9ba132a52f5f44a
jeon-ikhyun <ikjaeyo@icloud.com>
2023-12-08 22:55:21 +0900

    Second Commit

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -1,29 +1,69 @@
-import Reach, { SyntheticEvent } from "react";
+import Reach, { SyntheticEvent, useState } from "react";
 import { Project } from "./Project";
 
 interface ProjectFormProps {
+    project: Project;
     onSave: (project: Project) => void;
     onCancel: () => void;
 }
 
-function ProjectForm({ onSave, onCancel }: ProjectFormProps) {
+function ProjectForm({ project: initialProject, onSave, onCancel }: ProjectFormProps) {
+    const [project, setProject] = useState(initialProject);
+    const [errors, setErrors] = useState({
+        name: "",
+        description: "",
+        budget: ""
+    });
     const handleSubmit = (event: SyntheticEvent) => {
         event.preventDefault();
-        onSave(new Project({ name: "Updated Project" }));
+        onSave(project);
+    }
+    const handleChange = (event: any) => {
+        const { type, name, value, checked } = event.target;
+        let updatedValue = type === "checkbox" ? checked : value;
+
+        if (type === "number") {
+            updatedValue = Number(updatedValue);
+        }
+        const change = {
+            [name]: updatedValue,
+        };
+
+        let updatedProject: Project;
+        setProject((p) => {
+            updatedProject = new Project({ ...p, ...change });
+            return updatedProject;
+        });
+    }
+
+    function validate(project: Project) {
+        let errors: any = { name: "", description: "", bidguet: "" };
+        if (project.name.length === 0) {
+            errors.name = "Name is required";
+        }
+        if (project.name.length > 0 && project.name.length < 3) {
+            errors.name = "Name needs to be at least 3 characters.";
+        }
+        if (project.description.length === 0) {
+            errors.description = "Description is required.";
+        }
+        if (project.budget === 0) {
+            errors.budget = "Budget must be more than $0.";
+        }
     }
     
     return (
         <form className="input-group vertical" onSubmit={handleSubmit}>
             <label htmlFor="name">Project Name</label>
-            <input type="text" name="name" placeholder="enter name" />
+            <input type="text" name="name" placeholder="enter name" value={project.name} onChange={handleChange} />
             <label htmlFor="description">Project Description</label>
         
-            <textarea name="description" placeholder="enter description"></textarea>
+            <textarea name="description" placeholder="enter description" value={project.description} onChange={handleChange} />
             <label htmlFor="budget">Project Budget</label>
         
-            <input type="number" name="budget" placeholder="enter budget" />
+            <input type="number" name="budget" placeholder="enter budget" value={project.budget} onChange={handleChange} />
             <label htmlFor="isActive">Active?</label>
-            <input type="checkbox" name="isActive" />
+            <input type="checkbox" name="isActive" checked={project.isActive} onChange={handleChange} />
         
             <div className="input-group">
                 <button className="primary bordered medium">Save</button>
