65a67585ec562df81efdaa9944212779cb54be06
jeon-ikhyun <ikjaeyo@icloud.com>
2023-12-10 00:19:48 +0900

    API Commit

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -16,6 +16,9 @@ function ProjectForm({ project: initialProject, onSave, onCancel }: ProjectFormP
     });
     const handleSubmit = (event: SyntheticEvent) => {
         event.preventDefault();
+        if (!isValid()) {
+            return;
+        }
         onSave(project);
     }
     const handleChange = (event: any) => {
@@ -34,10 +37,11 @@ function ProjectForm({ project: initialProject, onSave, onCancel }: ProjectFormP
             updatedProject = new Project({ ...p, ...change });
             return updatedProject;
         });
+        setErrors(() => validate(updatedProject));
     }
 
     function validate(project: Project) {
-        let errors: any = { name: "", description: "", bidguet: "" };
+        let errors: any = { name: "", description: "", budget: "" };
         if (project.name.length === 0) {
             errors.name = "Name is required";
         }
@@ -50,18 +54,40 @@ function ProjectForm({ project: initialProject, onSave, onCancel }: ProjectFormP
         if (project.budget === 0) {
             errors.budget = "Budget must be more than $0.";
         }
+        return errors;
+    }
+
+    function isValid() {
+        return (
+            errors.name.length === 0 && errors.description.length === 0 && errors.budget.length === 0
+        );
     }
     
     return (
         <form className="input-group vertical" onSubmit={handleSubmit}>
             <label htmlFor="name">Project Name</label>
             <input type="text" name="name" placeholder="enter name" value={project.name} onChange={handleChange} />
+            {errors.name.length > 0 && (
+                <div className="card error">
+                    <p>{errors.name}</p>
+                </div>
+            )}
             <label htmlFor="description">Project Description</label>
         
             <textarea name="description" placeholder="enter description" value={project.description} onChange={handleChange} />
+            {errors.description.length > 0 && (
+                <div className="card error">
+                    <p>{errors.description}</p>
+                </div>
+            )}
             <label htmlFor="budget">Project Budget</label>
         
             <input type="number" name="budget" placeholder="enter budget" value={project.budget} onChange={handleChange} />
+            {errors.budget.length > 0 && (
+                <div className="card error">
+                    <p>{errors.budget}</p>
+                </div>
+            )}
             <label htmlFor="isActive">Active?</label>
             <input type="checkbox" name="isActive" checked={project.isActive} onChange={handleChange} />
         
--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,10 +1,31 @@
-import React, { Fragment, useState } from "react";
-import { MOCK_PROJECTS } from "./MockProjects";
+import React, { Fragment, useState, useEffect } from "react";
 import ProjectList from "./ProjectList";
 import { Project } from "./Project";
+import { projectAPI } from "./projectAPI";
 
 function ProjectsPage() {
-    const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+    const [projects, setProjects] = useState<Project[]>();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | undefined>(undefined);
+
+    useEffect(() => {
+        async function loadProjects() {
+            setLoading(true);
+            try {
+                const data = await projectAPI.get(1);
+                setError("");
+                setProjects(data);
+            } catch (e) {
+                if (e instanceof Error) {
+                    setError(e.message);
+                }
+            } finally {
+                setLoading(false);
+            }
+        }
+        loadProjects();
+    }, []);
+
     const saveProject = (project: Project) => {
         let updatedProjects = projects.map((p: Project) => {
             return p.id === project.id ? project : p;
