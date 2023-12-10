19e9895dd130fc417d7d86ba005c5c445db3340b
jeon-ikhyun <ikjaeyo@icloud.com>
2023-12-10 22:26:30 +0900

    API Commit

--- src/projects/projectAPI.ts
+++ src/projects/projectAPI.ts
@@ -1,6 +1,6 @@
 import { Project } from "./Project";
-const baseUrl = "http:localhost:4000";
-const url = "${baseUrl}/projects";
+const baseUrl = "http://localhost:4000";
+const url = `${baseUrl}/projects`;
 
 function translatesStatusToErrorMessage(status: number) {
     switch (status) {
@@ -50,8 +50,7 @@ function convertToProjectModel(item: any): Project {
 
 const projectAPI = {
     get(page = 1, limit = 20) {
-        return fetch("{url}?_page=${page}&_limit=${limit}&_sort=name")
-        .then(delay(600))
+        return fetch(`${url}?_page=${page}&_limit=${limit}&_sort=name`)
         .then(checkStatus)
         .then(parseJSON)
         .then(convertToProjectModels)
@@ -61,6 +60,31 @@ const projectAPI = {
                 "There was an error retrieving the projects. Please try again."
             );
         });
+    },
+
+    put(project: Project) {
+        return fetch(`${url}/${project.id}`, {
+            method: "PUT",
+            body: JSON.stringify(project),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(checkStatus)
+        .then(parseJSON)
+        .catch((error: TypeError) => {
+            console.log("log client error " + error);
+            throw new Error(
+                "There was an error retrieving the projects. Please try again."
+            );
+        });
+    },
+
+    find(id: number) {
+        return fetch(`${url}/${id}`)
+            .then(checkStatus)
+            .then(parseJSON)
+            .then(convertToProjectModel);
     },
 };
 
